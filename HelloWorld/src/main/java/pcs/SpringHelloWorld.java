3cadcd60bb2a08ed82c3c08dc67da10cac09c076
Paul Sprayberry <paul.sprayberry@imcva.com>
2022-08-02 17:06:04 -0500

    Add initial code

---
+++ HelloWorld/src/main/java/pcs/SpringHelloWorld.java
@@ -0,0 +1,45 @@
+// *****************************************************************************
+//   File:  SpringHelloWorld.java
+// *****************************************************************************
+/*
+ *   Declare the package
+ */
+package pcs;
+/*
+ *   Define types
+ */
+// =============================================================================
+//   Type:  SpringHelloWorld
+/*
+ *   Defines the main part of a Hello World application that uses the Spring 
+ *   framework.  This was an attempt to see if I could build a Spring Boot 
+ *   application from scratch.
+ */
+// =============================================================================
+public final 
+class SpringHelloWorld
+{ // begin SpringHelloWorld
+   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   //   Declare public features
+   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   /*
+    *   Define methods
+    */
+   public static final
+   void
+   main(final String[] notUsed)
+   { // begin running a SpringHelloWorld instance
+      throw new UnsupportedOperationException("SpringHelloWorld.main() is not available yet");
+   } // end running a SpringHelloWorld instance
+   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   //   Declare private features
+   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   /*
+    *   Define constructors
+    */
+   private
+   SpringHelloWorld()
+   { // begin constructing a SpringHelloWorld instance
+      throw new UnsupportedOperationException("SpringHelloWorld.<init> is not available yet");
+   } // end constructing a SpringHelloWorld instance
+} // end SpringHelloWorld
