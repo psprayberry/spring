53ba4b56f158274d4c4b5ed57619454e9fd5e2db
Paul Sprayberry <paul.sprayberry@imcva.com>
2022-08-07 16:11:55 -0500

    Add courses page

--- eazybytes-school/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
+++ eazybytes-school/src/main/java/com/eazybytes/eazyschool/EazyschoolApplication.java
@@ -10,23 +10,33 @@ package com.eazybytes.eazyschool;
  */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /*
  *   Define types
  */
 // =============================================================================
 //   Type:  EazyschoolApplication
-/*
+/**
  *   Runs the EazyBytes school web application via Spring Boot.
  * 
  *   @see SpringBootApplication
  */
+/*
+ *   XXX 2022-08-07 pcs Should a new type be created for the @Configuration 
+ *                      annotation?
+ *   XXX 2022-08-07 pcs Should a new type be created for the @Controller 
+ *                      annotation?
+ */
 // =============================================================================
+@Configuration
 @Controller
 @SpringBootApplication
 public
-class EazyschoolApplication
+class EazyschoolApplication implements WebMvcConfigurer
 { // begin the Eazy school application
    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    //   Define public features
@@ -36,7 +46,23 @@ class EazyschoolApplication
     */
    // --------------------------------------------------------------------------
    //   Method:  EazyschoolApplication.main
-   /*
+   /**
+    *   Wire in pages without specific controllers.
+    *
+    *   @param to the {@link ViewControllerRegistry} to which to add pages
+    */
+   // --------------------------------------------------------------------------
+   @Override
+   public final 
+   void 
+   addViewControllers(final ViewControllerRegistry to)
+   { // begin adding view controllers
+      to.addViewController("/about")  .setViewName("about");
+      to.addViewController("/courses").setViewName("courses");
+   } // end adding view controllers
+   // --------------------------------------------------------------------------
+   //   Method:  EazyschoolApplication.main
+   /**
     *   Runs the application.
     *
     *   @param args an array of {@link String}s containing the command line 
@@ -57,7 +83,7 @@ class EazyschoolApplication
     */
    // --------------------------------------------------------------------------
    //   Constructor:  EazyschoolApplication
-   /*
+   /**
     *   Constructs a default instance.
     */
    // --------------------------------------------------------------------------
@@ -71,9 +97,10 @@ class EazyschoolApplication
     */
    // --------------------------------------------------------------------------
    //   Constructor:  EazyschoolApplication.hello
-   /*
+   /**
     *   Displays a welcome message in a browser.
     */
+   /* */
    // --------------------------------------------------------------------------
    @RequestMapping(value = {"", "/", "home"})
    protected final 
