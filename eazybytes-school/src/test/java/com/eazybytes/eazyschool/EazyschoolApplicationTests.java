6fdcf57fa9abcbadf68f3e6fa1359969317512f0
Paul Sprayberry <paul.sprayberry@imcva.com>
2022-08-07 16:09:22 -0500

    Add tests

---
+++ eazybytes-school/src/test/java/com/eazybytes/eazyschool/EazyschoolApplicationTests.java
@@ -0,0 +1,63 @@
+// *****************************************************************************
+//   File:  EazyschoolApplicationTests.java
+// *****************************************************************************
+/*
+ *   Declare the package
+ */
+package com.eazybytes.eazyschool;
+/*
+ *   Import external features
+ */
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+ *   Define types
+ */
+// =============================================================================
+//   Type:  EazyschoolApplicationTests
+/**
+ *   Tests the Eazyschool application.
+ * 
+ *   @see EazyschoolApplication
+ */
+// =============================================================================
+@SpringBootTest
+public final 
+class EazyschoolApplicationTests 
+{ // begin testing the Eazyschool application
+   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   //   Declare protected features
+   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   /*
+    *   Define constructors
+    */
+   // --------------------------------------------------------------------------
+   //   Constructor:  EazyschoolApplicationTests
+   /**
+    *   Constructs a default test instance.
+    */
+   // --------------------------------------------------------------------------
+   protected
+   EazyschoolApplicationTests()
+   { // begin creating a test instance
+      super();
+   } // end creating a test instance
+   /*
+    *   Define methods
+    */
+   // --------------------------------------------------------------------------
+   //   Method:  EazyschoolApplicationTests.contextLoads
+   /**
+    *   This triggers some tests for the web application.
+    */
+   // --------------------------------------------------------------------------
+   @Test
+   protected final 
+   void 
+   contextLoads()
+   { // begin testing context loads
+      /*
+       *   Do nothing, but tests are actually invoked
+       */
+   } // end testing context loads
+} // end testing the Eazyschool application
